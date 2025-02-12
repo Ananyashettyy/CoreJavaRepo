e991c19b6bec376e4609d439b3b03c54153b1f6e
Ananyashettyy <141510840+Ananyashettyy@users.noreply.github.com>
2025-02-12 19:59:58 +0530

    Update evenOdd.java

--- corejava/evenOdd.java
+++ corejava/evenOdd.java
@@ -1,6 +1,9 @@
+import java.Scannar;
 public class evenOdd{
 	public static void main(String args[]){
-		int num= 23;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the number");
+		int num = sc.nextInt();
 		if(num % 2 == 0){
 			System.out.println(num + " is even number.");
 		}
