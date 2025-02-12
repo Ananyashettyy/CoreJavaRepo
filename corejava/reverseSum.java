0a7818a3203c90a81ac31d90155a5c418a7d3f13
Ananyashettyy <141510840+Ananyashettyy@users.noreply.github.com>
2025-02-12 20:17:47 +0530

    Update reverseSum.java

--- corejava/reverseSum.java
+++ corejava/reverseSum.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
 public class reverseSum{
 	public static void main(String args[]){
 		int num,rem;
 		int rev=0,sum=0;
-		num=123;
-		do {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int num=sc.nextInt();
+		while(num>0) {
 			rem=num % 10;
 			rev= rev * 10 +rem;
 			sum= sum + rem;
 			num=num/10;
-		}while(num>0);
+		}
 		System.out.println("The reverse of the number is "+rev);
 		System.out.println("The sum of the number is "+sum);
 	}
