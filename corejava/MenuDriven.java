d586b34fcb18ec913695b79306be962928e2f803
Ananya Shetty <ananyashetty0924.com>
2025-02-20 17:06:51 +0530

    Exception handling

--- corejava/MenuDriven.java
+++ corejava/MenuDriven.java
@@ -1,5 +1,4 @@
-
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Student{
@@ -8,7 +7,7 @@ class Student{
     int rollNo;
     int marks;
 
-    public Student(int id, String name, int rollNo, int marks){
+    Student(int id, String name, int rollNo, int marks){
         this.id = id;
         this.name = name;
         this.rollNo = rollNo;
@@ -16,99 +15,66 @@ class Student{
     }
 }
 class StudentDetail{
-   private Student[] students;
-    int size = 0;
-    int i;
+    static ArrayList<Student> students = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
-
-    public void addStudent(Student[] students) {
-        try {
-            if (size >= students.length) {
-                throw new CustomExceptions("Array is full");
-            }
-
+    public void addStudent(){
         System.out.println("Enter the id of the student: ");
         int id = sc.nextInt();
         System.out.println("Enter the name of the student: ");
         String name = sc.next();
-        System.out.println("Enter the roll number of the student: ");
+        System.out.println("Enter the roll number of the number: ");
         int rollNo = sc.nextInt();
         System.out.println("Enter the marks of the student: ");
         int marks = sc.nextInt();
-        students[size] = new Student(id, name, rollNo, marks);
-        size++;
-        System.out.println("Students added");
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
+        students.add(new Student(id, name, rollNo, marks));
+        System.out.println("Student added. ");
     }
-
-    public void deleteStudent(Student[] students) {
-        System.out.println("enter the student to be deleted");
-        int del = sc.nextInt();
-        boolean flag = false;
-        if (size == 0) {
-            System.out.println("the rollno to delete");
-            return;
-        }
-        for (i = 0; i < size; i++) {
-
-            if (students[i].rollNo == del) {
-               // students[i] = null;
-               // size--;
-                flag = true;
-                for(int j=i;j<size - 1;j++) {
-
-                    students[j] = students[j+1];
-                    students[j+1]=null;
-                }
-                students[size - 1]=null;
-                size--;
-                System.out.println("student deleted");
-            }
-        }
+    public void deleteStudent() {
+        System.out.println("enter the roll number");
+        int rolldelete = sc.nextInt();
+        boolean removed = students.removeIf(student -> student.rollNo == rolldelete);
+        if (removed) {
+            System.out.println("Student deleted.");
+        } else
+            System.out.println("Student not found.");
     }
-    public void readStudent(Student[] students){
-        if (size==0){
-            System.out.println("No students to display");
-        }else{
-            System.out.println("List of Students"+size);
-            for(i=0;i<size;i++){
-                System.out.println("student id:"+students[i].id+"\nStudent name:"+students[i].name+"\nStudent rollNo:"+students[i].rollNo+"\nStudents marks:"+students[i].marks);
+    public void readStudent(){
+        if(students.isEmpty()){
+            System.out.println("Array list is empty");
+        }
+        else{
+            for(Student student : students){
+                System.out.println("ID:"+student.id+"\nName: "+student.name+"\nRollNo: "+student.rollNo+"\nMarks: "+student.marks);
             }
         }
     }
 }
-class CustomExceptions extends  Exception{
-    public CustomExceptions(String message){
-        super(message);
-    }
-}
-public class MenuDriven{
-    public static void main(String[] args) throws CustomExceptions{
-        Student[] students = new Student[3];
-        Scanner sc = new Scanner(System.in);
-        StudentDetail studentDetail= new StudentDetail();
+public class MenuDriven {
+    public static void main(String[] args) {
         int option;
+        Scanner sc = new Scanner(System.in);
+        StudentDetail stud = new StudentDetail();
         do {
-            System.out.println("1. Add student");
-            System.out.println("2. Read student");
-            System.out.println("3. Remove student");
-            System.out.println("4. Exit");
+            System.out.println("Enter your choice\n 1. Add student\n 2. Read student\n 3. Remove student\n 4. Exit");
             option = sc.nextInt();
-            switch(option){
-                case 1: studentDetail.addStudent(students);
+
+            switch (option) {
+                case 1:
+                    stud.addStudent();
                     break;
-                case 2: studentDetail.readStudent(students);
+                case 2:
+                    stud.readStudent();
                     break;
-                case 3:studentDetail.deleteStudent(students);
+                case 3:
+                    stud.deleteStudent();
                     break;
-                case 4: System.out.println("exit");
-                    break;
-                default:System.out.println("Invalid option please try again");
-                    break;
+                case 4:
+                    System.out.println("Exited");
+                    return;
+                default:
+                    System.out.println("Invalid.");
             }
-        }while(option!=4);
+        }while(option != 4);
+
     }
 }
